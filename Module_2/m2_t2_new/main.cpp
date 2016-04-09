1011ce2f7fa5ecb28c639ec52e681dcf6413bbd3
iliabulavintsev <twintipper@mail.ru>
2016-04-09 10:08:44 +0300

    sem 3

--- Module_2/m2_t2_new/main.cpp
+++ Module_2/m2_t2_new/main.cpp
@@ -1,77 +1,69 @@
 #include <iostream>
 #include "assert.h"
 
+template <typename T>
+void siftDown(T *arr, int index, int size);
 class Heap{
 public:
-    Heap(int n): realsize(n), size(n), heap(new int[n]) {};
+    Heap(int n): realsize(n), size(n), heap(new long long[n]) {};
     ~Heap() {}
     int size;
     int realsize;
-    int *heap;
-    void SiftUp(int);
-    void AddElem(int);
-    int DelLastElem();
+    long long *heap;
+    void SiftDown(int);
+    void AddElem(long long);
+    long long DelFirstElem();
 };
 
-void Heap::AddElem(int elem) {
+void Heap::AddElem(long long elem) {
     assert(realsize < size);
-    heap[realsize] = elem;
+    heap--;
+    heap[0] = elem;
     realsize++;
+    SiftDown(0);
 }
 
-int Heap::DelLastElem() {
+long long Heap::DelFirstElem() {
     assert(realsize > 0);
+    long long ret = heap[0];
     realsize--;
-    return heap[realsize];
+    heap++;
+    return ret;
 }
 
-void Heap::SiftUp(int index) {
-    for (int i = index; i > 0;) {
-        if (heap[i] > heap[(i - 1)/2]){
-            std::swap(heap[i], heap[(i - 1)/2]);
-            i = (i - 1)/2;
+void Heap::SiftDown(int index) {
+    for (int i = index; i < realsize - 2;) {
+        int tmp = i;
+        int left = i*2 + 1;
+        int right = i*2 + 2;
+        if (heap[left] < heap[tmp] && left < realsize){
+            i = left;
         }
-        else break;
+        if (heap[right] < heap[tmp] && right < realsize){
+            i = right;
+        }
+        if (tmp == i)
+            break;
+        else
+            std::swap(heap[i], heap[tmp]);
+
     }
 }
 
-int count(Heap *heap){
-    int res = 0;
-    int a, b, c;
-    while (heap->realsize > 1){
-        if (heap->realsize % 2 || heap->realsize == 2) {
-            a = heap->DelLastElem();
-            b = heap->DelLastElem();
-        } else  {
-            a = heap->DelLastElem();
-            b = heap->DelLastElem();
-            c = heap->DelLastElem();
-            if (b < c){
-                heap->AddElem(c);
-            } else {
-                heap->AddElem(b);
-                b = c;
-            }
-        }
-        res+= a + b;
-        heap->AddElem(a+b);
-        heap->SiftUp(heap->realsize - 1);
-    }
-    return res;
-}
+
 template <typename T>
-void siftUp(T *arr, int index, int size){
+void siftDown(T *arr, int index, int size){
     for (int i = index; 2*i+2 <= size;) {
         int left = 2*i + 1;
         int right = 2*i + 2;
-        int min = i;
-        if (left < size && arr[min] > arr[left])
-            min = left;
-        if (right < size && arr[min] > arr[right])
-            min = right;
-        if (i != min){
-            std::swap(arr[i], arr[min]);
-            i = min;
+        int max = i;
+        if (left < size && arr[max] < arr[left])
+            max = left;
+        if (right < size && arr[max] < arr[right])
+            max = right;
+        if (i != max){
+            std::swap(arr[i], arr[max]);
+            i = max;
         } else
             break;
     }
@@ -79,53 +71,37 @@ void siftUp(T *arr, int index, int size){
 template <typename T>
 void heapSort(T *arr, int size){
     for (int i = size/2 - 1; i >= 0 ; --i) {
-        siftUp(arr, i, size);
+        siftDown(arr, i, size);
     }
     for (int j = 0; j < size; ++j) {
         std::swap(arr[0], arr[size - j - 1]);
-        siftUp(arr, 0, size - j - 1);
+        siftDown(arr, 0, size - j - 1);
     }
 }
-//
-//long long count(long long* arr, int size){
-//    heapSort(arr, size);
-////    for (int i = 0; i <size ; ++i) {
-////        std::cout<< arr[i]<<" ";
-////    }
-////    std::cout<<"_________\n";
-//    long long result = 0;
-//    for (int j = size - 1; j > 0 ;) {
-//        long long a = arr[j];
-//        long long b = arr[--j];
-//        result += a + b;
-//        arr[j] = a + b;
-//        heapSort(arr, j+1);
-//    }
-//    return result;
-//}
-
+long long count(Heap *heap){
+    long long res = 0;
+    long long a, b;
+    while (heap->realsize > 1){
+        a = heap->DelFirstElem();
+        b = heap->DelFirstElem();
+        res+= a + b;
+        heap->AddElem(a+b);
+    }
+    return res;
+}
 int main() {
     int n = 0;
     std::cin >> n;
     assert(n >= 0);
     Heap heap(n);
     for (int i = 0; i < n; ++i) {
-        int buf;
+        long long buf;
         std::cin >> buf;
         if(buf < 0)
             buf = 0 - buf;
         heap.heap[i] = buf;
     }
     heapSort(heap.heap, heap.realsize);
-//    for (int j = 0; j < heap.realsize ; ++j) {
-//        std::cout << heap.heap[j] << " _ ";
-//    }
     std::cout << count(&heap);
-
-//    int *arr = new int[n];
-//    for (int i = 0; i < n; ++i) {
-//        std::cin >> arr[i];
-//    }
-    //std::cout << count(arr, n);
     return 0;
 }
